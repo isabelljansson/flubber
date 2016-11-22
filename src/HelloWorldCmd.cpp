0debffffcba552ab613cc8cf55d89a325f6ddbf0
jonathanbosson <jonathan.bosson@gmail.com>
2016-11-22 16:07:10 +0100

    Removed helloWorld

--- src/HelloWorldCmd.cpp
+++
@@ -1,8 +0,0 @@
-#include "../include/HelloWorldCmd.h"
-
-void* HelloWorld::creator() { return new HelloWorld; }
- 
-MStatus HelloWorld::doIt(const MArgList& arg_list) {
-  MGlobal::displayInfo("Hello World! :)");
-  return MS::kSuccess;
-}
