c2247ce057c960ef43bac67142395bb5036a3e97
Isabell Jansson <janssonisabell@gmail.com>
2016-11-22 15:37:20 +0100

    included missing files

--- src/HelloWorldCmd.cpp
+++ src/HelloWorldCmd.cpp
@@ -3,6 +3,6 @@
 void* HelloWorld::creator() { return new HelloWorld; }
  
 MStatus HelloWorld::doIt(const MArgList& arg_list) {
-  MGlobal::displayInfo("Hello World!");
+  MGlobal::displayInfo("Hello World! :)");
   return MS::kSuccess;
 }
