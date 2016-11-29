4efe00d386261903a08d06129b5143229e2347aa
ronjagrosz <ronja_ronja@msn.com>
2016-11-29 08:48:26 +0100

    startup stuff

--- src/ParticleSystem.cpp
+++ src/ParticleSystem.cpp
@@ -1,15 +1,19 @@
 #include "../include/ParticleSystem.h"
 
 ParticleSystem::ParticleSystem(vector< vec3 >* x, vec3 vel) {
+    cout << "hej\n";
     // Set initial position
     x0 = x;
 
+    cout << "hej0\n";
     // Set initial velocity vector
     v->resize(x0->size());
     fill(v->begin(), v->end(), vel);
+    cout << "hej1\n";
 
     // Calculate initial center of mass
     initCom = calcCom(x0);
+    cout << "hej2\n";
 
 }
 
