686ec9c47ada9d816c067098b50e809a41a922fc
ronjagrosz <ronja_ronja@msn.com>
2016-12-06 15:39:43 +0100

    Might be appropriate main for testing

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,24 @@ int main(int argc, char* argv[]) {
     pos->push_back(glm::dvec3(1.0, 1.0, 0.0));
     glm::dvec3 vel = glm::dvec3(1, 0, 1);
     ParticleSystem object = ParticleSystem(pos, vel);
+    object.gravity = glm::dvec3(0.0, -9.82, 0.0);
+    object.initVel = glm::dvec3();
+    object.mass = 1.0;
+    object.flubbiness = 0.5;
+    object.friction = 0.1;
+    object.beta = 0.5;
+    object.elasticity = 0.5;
+    object.dt = 0.1;
+
+    for (int i = 0; i < 3; i++) {
+        object.applyForces();
+        object.deform();
+    }
+
+    // Write out new positions
+    for ( int i = 0; i < pos->size(); i++) {
+        cout << "pos: " << glm::to_string(object.getPosition(i)) << endl;
+    }
 
     cout << "Weeee" << endl;
     delete pos;
